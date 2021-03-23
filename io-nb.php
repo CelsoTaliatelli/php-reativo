a9627ca9b396fc9dc5b765081dd869e9881a2058
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-03-23 20:37:24 -0300

    Abrindo um cliente

--- io-nb.php
+++ io-nb.php
@@ -2,10 +2,11 @@
 
 //Lista de Mensagens
 $streamList = [
+    stream_socket_client('tcp://localhost:8080'), //retorna um stream
     fopen('arquivo1.txt','r'),
     fopen('arquivo2.txt','r')
 ];  
-
+fwrite($streamList[0],'GET /http-server.php HTTP/1.1'.PHP_EOL.PHP_EOL);
 foreach($streamList as $stream){
     stream_set_blocking($stream,false);
 }
@@ -20,7 +21,13 @@ do{
     }
 
     foreach($copyReadStream as $key => $stream){
-        echo fgets($stream);
+        $conteudo = stream_get_contents($stream);
+        $posicaoFimHttp = strpos($conteudo,"\r\n\r\n");
+        if($posicaoFimHttp !== false){
+            echo substr($conteudo,$posicaoFimHttp + 4);
+        }else{
+            echo $conteudo;
+        }
         unset($streamList[$key]);
     }
 
